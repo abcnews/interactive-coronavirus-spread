3a82e8857c9defd419945e74c80ea5ca16d2284e
Colin Gourlay <gourlay.colin@abc.net.au>
2020-03-13 13:59:59 +1000

    Add data URLs

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const DATA_URL = 'https://www.abc.net.au/dat/news/interactives/covid19-data/data.json';
+export const COUNTRY_TOTALS_URL = 'https://www.abc.net.au/dat/news/interactives/covid19-data/country-totals.json';
