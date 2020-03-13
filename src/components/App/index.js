ae70485692d1c4bb9112f8393d7ecae435cba71f
Simon Elvery <simon@elvery.net>
2020-03-13 12:12:49 +1000

    Integrate scrollyteller

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.scss";
-import worm from "./worm.svg";
+import Scrollyteller from "@abcnews/scrollyteller";
 
-import Scrollyteller, { loadScrollyteller } from "@abcnews/scrollyteller";
+export default ({ scrollyData }) => {
+  const [booms, setBooms] = useState(0);
 
-const scrollyData = loadScrollyteller(
-  "", // If set to eg. "one" use #scrollytellerNAMEone in CoreMedia
-  "u-full", // Class to apply to mount point u-full makes it full width in Odyssey
-  "mark" // Name of marker in CoreMedia eg. for "point" use #point default: #mark
-);
-
-export default props => {
   return (
-    <Scrollyteller panels={scrollyData.panels} onMarker={console.log}>
-      <div className={styles.root}>
-        <img className={styles.worm} src={worm} />
-        <h1>{props.projectName}</h1>
-      </div>
+    <Scrollyteller panels={scrollyData.panels}>
+      <div>{booms || "no booms"}</div>
     </Scrollyteller>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import { loadScrollyteller } from "@abcnews/scrollyteller";
 
 const PROJECT_NAME = "interactive-coronavirus-spread";
 const root = document.querySelector(`[data-${PROJECT_NAME}-root]`);
 
 function init() {
-  render(<App projectName={PROJECT_NAME} />, root);
+  let scrollyData;
+  try {
+    scrollyData = loadScrollyteller("one", "u-full");
+  } catch (e) {
+    console.error(e);
+  }
+
+  // Keep the DOM tidy.
+  if (scrollyData && scrollyData.mountNode) {
+    while (scrollyData.mountNode.nextElementSibling.tagName === "A") {
+      window.__ODYSSEY__.utils.dom.detach(
+        scrollyData.mountNode.nextElementSibling
+      );
+    }
+  }
+
+  render(<App scrollyData={scrollyData} />, scrollyData.mountNode);
 }
 
-init();
+if (window.__ODYSSEY__) {
+  init();
+} else {
+  window.addEventListener("odyssey:api", init);
+}
 
 if (module.hot) {
   module.hot.accept("./components/App", () => {
