1b9116b447b37157c05863e1b268b6a7802e8bc3
Colin Gourlay <gourlay.colin@abc.net.au>
2020-03-13 14:04:23 +1000

    Manage initialisation & hot reloading, passing data into App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
-import styles from "./styles.scss";
-import Scrollyteller from "@abcnews/scrollyteller";
+import Scrollyteller from '@abcnews/scrollyteller';
+import React, { useCallback, useState } from 'react';
+import styles from './styles.scss';
 
-export default ({ scrollyData }) => {
+export default ({ scrollyData, countryTotals }) => {
   const [booms, setBooms] = useState(0);
 
+  console.debug(countryTotals);
+
+  const onMarker = useCallback(config => {
+    const { booms } = config;
+
+    setBooms(booms);
+  }, []);
+
   return (
-    <Scrollyteller panels={scrollyData.panels}>
-      <div>{booms || "no booms"}</div>
+    <Scrollyteller panels={scrollyData.panels} onMarker={onMarker}>
+      <div>{booms || 'no booms'}</div>
     </Scrollyteller>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,43 +1,59 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./components/App";
-import { loadScrollyteller } from "@abcnews/scrollyteller";
+import { loadScrollyteller } from '@abcnews/scrollyteller';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import { COUNTRY_TOTALS_URL } from './constants';
 
-const PROJECT_NAME = "interactive-coronavirus-spread";
+const PROJECT_NAME = 'interactive-coronavirus-spread';
 const root = document.querySelector(`[data-${PROJECT_NAME}-root]`);
 
-function init() {
-  let scrollyData;
-  try {
-    scrollyData = loadScrollyteller("one", "u-full");
-  } catch (e) {
-    console.error(e);
-  }
+function renderApp(scrollyData, countryTotals) {
+  render(<App scrollyData={scrollyData} countryTotals={countryTotals} />, scrollyData.mountNode);
+}
 
-  // Keep the DOM tidy.
-  if (scrollyData && scrollyData.mountNode) {
-    while (scrollyData.mountNode.nextElementSibling.tagName === "A") {
-      window.__ODYSSEY__.utils.dom.detach(
-        scrollyData.mountNode.nextElementSibling
-      );
+const whenOdysseyLoaded = new Promise(resolve =>
+  window.__ODYSSEY__
+    ? resolve(window.__ODYSSEY__)
+    : window.addEventListener('odyssey:api', () => resolve(window.__ODYSSEY__))
+);
+
+const whenScrollytellerLoaded = new Promise((resolve, reject) =>
+  whenOdysseyLoaded.then(odyssey => {
+    let scrollyData;
+
+    try {
+      scrollyData = loadScrollyteller('one', 'u-full');
+    } catch (err) {
+      reject(err);
     }
-  }
 
-  render(<App scrollyData={scrollyData} />, scrollyData.mountNode);
+    // Keep the DOM tidy.
+    if (scrollyData && scrollyData.mountNode) {
+      while (scrollyData.mountNode.nextElementSibling.tagName === 'A') {
+        odyssey.utils.dom.detach(scrollyData.mountNode.nextElementSibling);
+      }
+    }
+
+    resolve(scrollyData);
+  })
+);
+
+const whenCountryTotalsFetched = fetch(COUNTRY_TOTALS_URL).then(response => response.json());
+
+function renderAppWhenReady() {
+  Promise.all([whenScrollytellerLoaded, whenCountryTotalsFetched])
+    .then(results => renderApp.apply(null, results))
+    .catch(console.error);
 }
 
-if (window.__ODYSSEY__) {
-  init();
-} else {
-  window.addEventListener("odyssey:api", init);
-}
+renderAppWhenReady();
 
 if (module.hot) {
-  module.hot.accept("./components/App", () => {
+  module.hot.accept('./components/App', () => {
     try {
-      init();
+      renderAppWhenReady();
     } catch (err) {
-      import("./components/ErrorBox").then(exports => {
+      import('./components/ErrorBox').then(exports => {
         const ErrorBox = exports.default;
         render(<ErrorBox error={err} />, root);
       });
@@ -45,6 +61,6 @@ if (module.hot) {
   });
 }
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   console.debug(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
 }
