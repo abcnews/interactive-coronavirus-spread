eb9991e39cad5e98649e5ed19121aef9ca6d4ae1
Colin Gourlay <gourlay.colin@abc.net.au>
2020-03-17 14:16:30 +1000

    Roughly map out presets for the cases chart, to follow draft script in Core

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,46 @@
 export const DATA_URL = 'https://www.abc.net.au/dat/news/interactives/covid19-data/data.json';
 export const COUNTRY_TOTALS_URL = 'https://www.abc.net.au/dat/news/interactives/covid19-data/country-totals.json';
 
+const KEY_COUNTRIES = ['Australia', 'China', 'Italy', 'Singapore', 'South Korea', 'UK', 'US'];
+
 export const PRESETS = {
   initial: {
     graphic: 'cases',
     xScaleType: 'dates',
-    yScaleType: 'linear'
+    yScaleType: 'linear',
+    countries: true,
+    trends: false
+  },
+  china: {
+    graphic: 'cases',
+    xScaleType: 'dates',
+    yScaleType: 'linear',
+    countries: true,
+    highlightedCountries: ['China'],
+    trends: true
+  },
+  all: {
+    graphic: 'cases',
+    xScaleType: 'dates',
+    yScaleType: 'linear',
+    countries: true,
+    trends: true
+  },
+  key: {
+    graphic: 'cases',
+    xScaleType: 'dates',
+    yScaleType: 'linear',
+    countries: KEY_COUNTRIES,
+    highlightedCountries: true,
+    trends: true
   },
   hundred: {
     graphic: 'cases',
     xScaleType: 'days',
-    yScaleType: 'linear'
+    yScaleType: 'linear',
+    countries: KEY_COUNTRIES,
+    highlightedCountries: true,
+    trends: true
   },
   doubling: {
     graphic: 'doubling'
@@ -18,6 +48,25 @@ export const PRESETS = {
   logarithmic: {
     graphic: 'cases',
     xScaleType: 'days',
-    yScaleType: 'logarithmic'
+    yScaleType: 'logarithmic',
+    countries: KEY_COUNTRIES,
+    highlightedCountries: true,
+    trends: false
+  },
+  trends: {
+    graphic: 'cases',
+    xScaleType: 'days',
+    yScaleType: 'logarithmic',
+    countries: KEY_COUNTRIES,
+    trends: true,
+    highlightedTrends: true
+  },
+  lowtrend: {
+    graphic: 'cases',
+    xScaleType: 'days',
+    yScaleType: 'logarithmic',
+    countries: KEY_COUNTRIES,
+    trends: true,
+    highlightedTrends: ['low']
   }
 };
