c813ee56b17802302d8628703d5cb7cb20beedf5
Colin Gourlay <gourlay.colin@abc.net.au>
2020-03-30 12:24:25 +1000

    We need countryTotals, of course

--- src/standalone-cases-graphic.js
+++ src/standalone-cases-graphic.js
@@ -4,4 +4,4 @@ const domready = fn => {
   /in/.test(document.readyState) ? setTimeout(() => domready(fn), 9) : fn();
 };
 
-fetchCountryTotals().then(() => domready(renderCasesGraphics));
+fetchCountryTotals().then(countryTotals => domready(() => renderCasesGraphics(countryTotals)));
