94cd6417c2b57d68655180308252e5d54989bd0f
Colin Gourlay <gourlay.colin@abc.net.au>
2020-05-25 15:00:59 +1000

    Update redirect dynamic JS on deploy (so it uses the correct id).

--- aunty.config.js
+++ aunty.config.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const { name, version } = require('./package.json');
 
 const ADDITIONAL_ENTRY_POINTS = [
   'standalone-cases-graphic',
@@ -20,20 +19,22 @@ module.exports = {
       config.entry[name] = [config.entry.index[0].replace('index.js', `${name}.js`)];
     });
 
-    fs.writeFileSync(
-      path.join(__dirname, 'redirect', 'index.js'),
-      `window.location = String(window.location).replace(/${name}\\/[\\w\\d\\.]+\\//, '${name}/${version}/')`
-    );
-
     return config;
   },
   deploy: [
     {
       to: '/www/res/sites/news-projects/<name>/<id>'
     },
-    {
-      from: 'redirect',
-      to: '/www/res/sites/news-projects/<name>/latest'
+    config => {
+      fs.writeFileSync(
+        path.join(__dirname, 'redirect', 'index.js'),
+        `window.location = String(window.location).replace('/latest/', '/${config.id}/')`
+      );
+
+      return {
+        from: 'redirect',
+        to: '/www/res/sites/news-projects/<name>/latest'
+      };
     }
   ]
 };
