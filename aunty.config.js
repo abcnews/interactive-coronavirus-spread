aeb92196065df064b97449a9f6e1e82421edf13e
Colin Gourlay <gourlay.colin@abc.net.au>
2020-05-25 14:47:09 +1000

    Add a redirect and additional deployment for /latest/

--- aunty.config.js
+++ aunty.config.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const { name, version } = require('./package.json');
+
 const ADDITIONAL_ENTRY_POINTS = [
   'standalone-cases-graphic',
   'cases-graphic-explorer',
@@ -16,6 +20,20 @@ module.exports = {
       config.entry[name] = [config.entry.index[0].replace('index.js', `${name}.js`)];
     });
 
+    fs.writeFileSync(
+      path.join(__dirname, 'redirect', 'index.js'),
+      `window.location = String(window.location).replace(/${name}\\/[\\w\\d\\.]+\\//, '${name}/${version}/')`
+    );
+
     return config;
-  }
+  },
+  deploy: [
+    {
+      to: '/www/res/sites/news-projects/<name>/<id>'
+    },
+    {
+      from: 'redirect',
+      to: '/www/res/sites/news-projects/<name>/latest'
+    }
+  ]
 };
